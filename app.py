0b5d8c0aaf2332e9dc82257c6fbe28a39a49778f
arian81 <ahmadinejadarian@gmail.com>
2022-10-31 22:54:39 -0400

    run on port var

--- app.py
+++ app.py
@@ -66,6 +66,7 @@ def getloc():
 
 
 # @app.route("")
-
+import os
+print(os.environ['PORT'])
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=True, host="0.0.0.0", port=os.environ['PORT'])
